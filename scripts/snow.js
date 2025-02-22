9368834f2dadb950bcbb6b0cbe0f8c51baffb50e
Nikita <mkrnlox@gmail.com>
2025-02-22 22:30:50 +0300

    made snowflake smaller

--- scripts/snow.js
+++ scripts/snow.js
@@ -6,9 +6,14 @@ document.addEventListener('DOMContentLoaded', function () {
 	for (let i = 0; i < 100; i++) {
 		const snowflake = document.createElement('div')
 		snowflake.className = 'snowflake'
+		snowflake.style.scale = `${Math.random(1) * 1 * 0.5}`
 		snowflake.style.left = `${Math.random() * 100}vw`
 		snowflake.style.animationDuration = `${Math.random() * 3 + 2}s`
 		snowflake.style.animationDelay = `${Math.random() * 5}s`
 		snowContainer.appendChild(snowflake)
+
+		snowflake.addEventListener('animationend', function () {
+			snowflake.remove()
+		})
 	}
 })
